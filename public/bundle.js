7c10c95c608375a794fea6c0f47caed985983c48
Miguel <miguel.martinez.mestre@alumnos.upm.es>
2021-01-28 06:15:54 +0100

    idea eliminado

---
+++ public/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("hola"),function o(n){return 0==n||1==n?0:n*o(n-1)}(1)})();
+//# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 console.log("hola")
+function f(n: number):number {
+   if (n==0 || n==1) return 0;
+   return n * f(n-1);
+}
+f(1)
